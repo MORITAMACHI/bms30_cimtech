d6d19db577c8fd5e86befeab73dabe664ae4e049
たけだり <107227273+riho-takeda@users.noreply.github.com>
2022-08-23 17:36:37 +0900

    Add files via upload

---
+++ bms30_cimtech/error/error.php
@@ -0,0 +1,14 @@
+<?php
+    $msg = $_GET['errorMsg'];
+?>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>Insert title here</title>
+	</head>
+    <body>
+    エラー<br>
+    <?=$msg?>
+
+    </body>
+</html>
