025b84bfb54f5647c2df18678383c5024d756df2
貝谷　和則 <107822127+kazunorikaitani@users.noreply.github.com>
2022-08-24 17:22:38 +0900

    Add files via upload

--- bms30_cimtech/menu/menu.php
+++ bms30_cimtech/menu/menu.php
@@ -12,12 +12,3 @@
 include("menu.phtml");
 
 ?>
-<html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		<title>Insert title here</title>
-	</head>
-    <body>
-
-    </body>
-</html>
